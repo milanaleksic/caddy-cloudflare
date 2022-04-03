e1b0c3bc6b8e9dc49962a2056f3346b53318742c
Milan Aleksić <milanaleksic@gmail.com>
2022-04-03 07:34:36 +0200

    handle signals since caddy does not ref: https://github.com/caddyserver/caddy/issues/3967

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,12 @@ RUN caddy-builder \
 FROM caddy:${CADDY_VERSION}
 
 COPY --from=builder /usr/bin/caddy /usr/bin/caddy
+
+# Reference:
+# 1. discussion: https://github.com/caddyserver/caddy/issues/3967
+# 1. implementation: https://github.com/optiz0r/caddy-consul/
+# Override the entrypoint with a bash script which handles SIGHUP and triggers reload
+RUN apk add --no-cache tini
+COPY signal-handler.sh /
+ENTRYPOINT ["/sbin/tini", "--"]
+CMD ["/signal-handler.sh", "caddy", "run", "--config", "/etc/caddy/Caddyfile", "--adapter", "caddyfile"]
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # latest on 10/02/2022
 export CADDY_VERSION=2.4.6
+export IMAGE_VERSION=2.4.6.1
 
 # This depends on Docker for Desktop (Mac), because that one supports multi-arch output
 # Create that builder with "docker buildx create --name multiarch"
@@ -10,8 +11,9 @@ docker buildx use multiarch
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
   --build-arg CADDY_VERSION=${CADDY_VERSION} \
-  -t milanaleksic/caddy-cloudflare:${CADDY_VERSION} \
+  -t milanaleksic/caddy-cloudflare:${IMAGE_VERSION} \
   --push \
   .
 
-# NOTE: this image can't be remo
+# NOTE: this image can't be moved to private registry since it would inflict chicken/egg problem
+# since the reverse proxy is based on this image :)
