c704c87df9abea47b79524e50b11b8c5244d5ff5
Milan Aleksić <milanaleksic@gmail.com>
2023-01-10 16:26:21 +0100

    use 2.6.2 caddy

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# latest on 10/02/2022
-export CADDY_VERSION=2.4.6
-export IMAGE_VERSION=2.4.6.1
+# latest on 10/01/2023
+export CADDY_VERSION=2.6.2
+export IMAGE_VERSION=2.6.2
 
 # This depends on Docker for Desktop (Mac), because that one supports multi-arch output
 # Create that builder with "docker buildx create --name multiarch"
